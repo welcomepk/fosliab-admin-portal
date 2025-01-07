44d88291e0ac63f51c6c1ceb3e134fa2495e8586
pramod_kamble <kamblepramod806@gmail.com>
2025-01-07 19:00:48 +0530

    table update

--- src/components/table/data-table.jsx
+++ src/components/table/data-table.jsx
@@ -18,34 +18,9 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
-import DataTableHead from './table/data-table-head';
+import DataTableHead from './data-table-head';
 
-function createData(id, name, calories, fat, carbs, protein) {
-    return {
-        id,
-        name,
-        calories,
-        fat,
-        carbs,
-        protein,
-    };
-}
 
-const rows = [
-    createData(1, 'Cupcake', 305, 3.7, 67, 4.3),
-    createData(2, 'Donut', 452, 25.0, 51, 4.9),
-    createData(3, 'Eclair', 262, 16.0, 24, 6.0),
-    createData(4, 'Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData(5, 'Gingerbread', 356, 16.0, 49, 3.9),
-    createData(6, 'Honeycomb', 408, 3.2, 87, 6.5),
-    createData(7, 'Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData(8, 'Jelly Bean', 375, 0.0, 94, 0.0),
-    createData(9, 'KitKat', 518, 26.0, 65, 7.0),
-    createData(10, 'Lollipop', 392, 0.2, 98, 0.0),
-    createData(11, 'Marshmallow', 318, 0, 81, 2.0),
-    createData(12, 'Nougat', 360, 19.0, 9, 37.0),
-    createData(13, 'Oreo', 437, 18.0, 63, 4.0),
-];
 
 function descendingComparator(a, b, orderBy) {
     if (b[orderBy] < a[orderBy]) {
@@ -63,38 +38,7 @@ function getComparator(order, orderBy) {
         : (a, b) => -descendingComparator(a, b, orderBy);
 }
 
-const headCells = [
-    {
-        id: 'name',
-        numeric: false,
-        disablePadding: true,
-        label: 'Dessert (100g serving)',
-    },
-    {
-        id: 'calories',
-        numeric: true,
-        disablePadding: false,
-        label: 'Calories',
-    },
-    {
-        id: 'fat',
-        numeric: true,
-        disablePadding: false,
-        label: 'Fat (g)',
-    },
-    {
-        id: 'carbs',
-        numeric: true,
-        disablePadding: false,
-        label: 'Carbs (g)',
-    },
-    {
-        id: 'protein',
-        numeric: true,
-        disablePadding: false,
-        label: 'Protein (g)',
-    },
-];
+
 
 
 
@@ -153,10 +97,9 @@ DataTableToolbar.propTypes = {
     numSelected: PropTypes.number.isRequired,
 };
 
-export default function DataTable() {
+export default function DataTable({ rows, headCells, selected = [], setSelected, selectable = true }) {
     const [order, setOrder] = React.useState('asc');
     const [orderBy, setOrderBy] = React.useState('calories');
-    const [selected, setSelected] = React.useState([]);
     const [page, setPage] = React.useState(0);
     const [dense, setDense] = React.useState(true);
     const [rowsPerPage, setRowsPerPage] = React.useState(5);
@@ -177,9 +120,9 @@ export default function DataTable() {
     };
 
     const handleClick = (event, id) => {
+        if (!selectable) return;
         const selectedIndex = selected.indexOf(id);
         let newSelected = [];
-
         if (selectedIndex === -1) {
             newSelected = newSelected.concat(selected, id);
         } else if (selectedIndex === 0) {
@@ -217,7 +160,7 @@ export default function DataTable() {
             [...rows]
                 .sort(getComparator(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
-        [order, orderBy, page, rowsPerPage],
+        [order, orderBy, page, rowsPerPage, rows],
     );
 
     return (
@@ -239,6 +182,7 @@ export default function DataTable() {
                             onRequestSort={handleRequestSort}
                             rowCount={rows.length}
                             headCells={headCells}
+                            selectable={selectable}
                         />
                         <TableBody>
                             {visibleRows.map((row, index) => {
@@ -254,22 +198,26 @@ export default function DataTable() {
                                         tabIndex={-1}
                                         key={row.id}
                                         selected={isItemSelected}
-                                        sx={{ cursor: 'pointer' }}
+                                        sx={{ cursor: selectable ? "pointer" : "default" }}
                                     >
-                                        <TableCell padding="checkbox">
-                                            <Checkbox
-                                                color="primary"
-                                                checked={isItemSelected}
-                                                inputProps={{
-                                                    'aria-labelledby': labelId,
-                                                }}
-                                            />
-                                        </TableCell>
+                                        {/* only visible if selectable is TRUE */}
+                                        {
+                                            selectable &&
+                                            <TableCell padding="checkbox">
+                                                <Checkbox
+                                                    color="primary"
+                                                    checked={isItemSelected}
+                                                    inputProps={{
+                                                        'aria-labelledby': labelId,
+                                                    }}
+                                                />
+                                            </TableCell>
+                                        }
                                         <TableCell
                                             component="th"
                                             id={labelId}
                                             scope="row"
-                                            padding="none"
+                                            padding={selectable ? "none" : "normal"}
                                         >
                                             {row.name}
                                         </TableCell>
@@ -302,10 +250,20 @@ export default function DataTable() {
                     onRowsPerPageChange={handleChangeRowsPerPage}
                 />
             </Paper>
-            <FormControlLabel
+            {/* <FormControlLabel
                 control={<Switch checked={dense} onChange={handleChangeDense} />}
                 label="Dense padding"
-            />
+            /> */}
         </Box>
     );
 }
+
+DataTable.propTypes = {
+    rows: PropTypes.array,
+    headCells: PropTypes.array,
+    selected: PropTypes.array,
+    setSelected: PropTypes.func,
+    selectable: PropTypes.bool
+}
+
+
--- src/pages/maintenance/super-scheme-maker.jsx
+++ src/pages/maintenance/super-scheme-maker.jsx
@@ -1,17 +1,87 @@
 
-import { Paper } from "@mui/material"
+import { TextField, Paper } from "@mui/material"
 import PropTypes from "prop-types"
 import PageWrapper from "../../components/page-wrapper"
-import DataTable from "../../components/data-table"
+import DataTable from "../../components/table/data-table"
+import { useState } from "react";
+
+const headCells = [
+    {
+        id: 'name',
+        numeric: false,
+        disablePadding: true,
+        label: 'Dessert (100g serving)',
+    },
+    {
+        id: 'calories',
+        numeric: true,
+        disablePadding: false,
+        label: 'Calories',
+    },
+    {
+        id: 'fat',
+        numeric: true,
+        disablePadding: false,
+        label: 'Fat (g)',
+    },
+    {
+        id: 'carbs',
+        numeric: true,
+        disablePadding: false,
+        label: 'Carbs (g)',
+    },
+    {
+        id: 'protein',
+        numeric: true,
+        disablePadding: false,
+        label: 'Protein (g)',
+    },
+];
+function filterItems(items, criteria) {
+    return items.filter(item =>
+        Object.keys(criteria).every(key =>
+            String(item[key]).toLowerCase().includes(String(criteria[key]).toLowerCase())
+        )
+    );
+}
+const items = [
+    { id: 1, name: 'Cupcake', calories: 305, fat: 3.7, carbs: 67, protein: 4.3 },
+    { id: 2, name: 'Donut', calories: 452, fat: 25.0, carbs: 51, protein: 4.9 },
+    { id: 3, name: 'Eclair', calories: 262, fat: 16.0, carbs: 24, protein: 6.0 },
+    { id: 4, name: 'Frozen yoghurt', calories: 159, fat: 6.0, carbs: 24, protein: 4.0 },
+    { id: 5, name: 'Gingerbread', calories: 356, fat: 16.0, carbs: 49, protein: 3.9 },
+    { id: 6, name: 'Honeycomb', calories: 408, fat: 3.2, carbs: 87, protein: 6.5 },
+    { id: 7, name: 'Ice cream sandwich', calories: 237, fat: 9.0, carbs: 37, protein: 4.3 },
+    { id: 8, name: 'Jelly Bean', calories: 375, fat: 0.0, carbs: 94, protein: 0.0 },
+    { id: 9, name: 'KitKat', calories: 518, fat: 26.0, carbs: 65, protein: 7.0 },
+    { id: 10, name: 'Lollipop', calories: 392, fat: 0.2, carbs: 98, protein: 0.0 },
+    { id: 11, name: 'Marshmallow', calories: 318, fat: 0, carbs: 81, protein: 2.0 },
+    { id: 12, name: 'Nougat', calories: 360, fat: 19.0, carbs: 9, protein: 37.0 },
+    { id: 13, name: 'Oreo', calories: 437, fat: 18.0, carbs: 63, protein: 4.0 }
+]
 
 function SuperSchemeMakerPage({ title }) {
+    const [selected, setSelected] = useState([]);  // for data-table
+    const [desert, setDesert] = useState("")
+    const filteredRows = filterItems(items, { name: desert })
+    console.log(filteredRows);
+
     title = "Super Scheme Master - Maker"
     return (
         <PageWrapper>
-            <Paper variant="outlined" className="p-2">
-                <h5 className="text-center p-2">{title}</h5>
+            <Paper variant="outlined" className="p-2" sx={{ backgroundColor: "#d8107b57", }}>
+                <h5 className="text-center m-0">{title}</h5>
             </Paper>
-            <DataTable />
+            <Paper variant="outlined" className="p-2">
+                <TextField onChange={e => setDesert(e.target.value)} size="small" id="dis" placeholder="search desert" label="Dessert" variant="filled" />
+            </Paper>
+            <DataTable
+                rows={filteredRows}
+                headCells={headCells}
+                selected={selected}
+                setSelected={setSelected}
+                selectable={false}
+            />
         </PageWrapper>
     )
 }
